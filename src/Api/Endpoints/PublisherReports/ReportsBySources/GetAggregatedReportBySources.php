de231333ab72f88ef9d94ef3f8bfcedc217011a9
Gleb Tuchinskii <gleb.tuchinskii@gmail.com>
2024-05-23 14:01:40 +0300

    feat: reports by sources (without doc)

---
+++ src/Api/Endpoints/PublisherReports/ReportsBySources/GetAggregatedReportBySources.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Affiliateforge\PhpAdmitadApi\Api\Endpoints\PublisherReports\ReportsBySources;
+
+use Affiliateforge\PhpAdmitadApi\Api\Endpoints\PublisherReports\GetAggregatedReportCommand;
+
+class GetAggregatedReportBySources extends GetReportsBySources
+{
+    use GetAggregatedReportCommand;
+}
---
+++ tests/Unit/Api/Endpoints/PublisherReports/ReportsBySources/GetReportsBySourcesTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Unit\Api\Endpoints\PublisherReports\ReportsBySources;
+
+use Affiliateforge\PhpAdmitadApi\Api\Endpoints\PublisherReports\ReportsBySources\GetReportsBySources;
+use Affiliateforge\PhpAdmitadApi\Api\Endpoints\PublisherReports\ReportsBySources\GetReportsBySourcesResponse;
+use Nyholm\Psr7\Factory\Psr17Factory;
+use PHPUnit\Framework\TestCase;
+
+class GetReportsBySourcesTest extends TestCase
+{
+    public function testEndpointConfiguration()
+    {
+        $dto = new GetReportsBySources(
+            offset: 8,
+            limit: 20,
+            dateStart: new \DateTime('2024-05-13 12:36:14'),
+            dateEnd: new \DateTime('2024-03-16 16:10:34'),
+            website: 48,
+            campaign: 74,
+            orderBy: 'corrupti',
+        );
+
+        $this->assertSame('/statistics/sources/', $dto->getUrlPath());
+        $this->assertSame('GET', $dto->getHttpMethod());
+        $this->assertEmpty($dto->getBody());
+        $this->assertSame([
+            'offset' => 8,
+            'limit' => 20,
+            'date_start' => '13.05.2024',
+            'date_end' => '16.03.2024',
+            'website' => 48,
+            'campaign' => 74,
+            'order_by' => 'corrupti',
+        ], $dto->getQueryParams());
+
+        $mockResponse = (new Psr17Factory())->createResponse();
+        $this->assertInstanceOf(GetReportsBySourcesResponse::class, $dto->makeResponseDTO($mockResponse));
+    }
+}
