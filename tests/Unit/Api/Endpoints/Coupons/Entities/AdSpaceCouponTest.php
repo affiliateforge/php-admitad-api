d8748c916822cdfeae4c955dfd52974d876920a9
Stanislav Surskiy <ssurskiy@gmail.com>
2024-06-16 01:15:04 +0400

    test(coupons): enhance `testGetters` with data provider
    - Add `gettersDataProvider` method to supply test data
    - Refactor `testGetters` to use data from `gettersDataProvider`
    - Move hardcoded JSON response in `testGetters` to the data fixtures
    - Improve assertions for existing fields to handle dynamic data

--- tests/Unit/Api/Endpoints/Coupons/Entities/AdSpaceCouponTest.php
+++ tests/Unit/Api/Endpoints/Coupons/Entities/AdSpaceCouponTest.php
@@ -6,53 +6,82 @@ use Affiliateforge\PhpAdmitadApi\Api\Endpoints\Coupons\Entities\AdSpaceCoupon;
 use Affiliateforge\PhpAdmitadApi\Api\Endpoints\Coupons\Entities\Campaign;
 use Affiliateforge\PhpAdmitadApi\Api\Endpoints\Coupons\Entities\Category;
 use Affiliateforge\PhpAdmitadApi\Api\Endpoints\Coupons\Entities\Type;
+use Exception;
+use Generator;
 use PHPUnit\Framework\TestCase;
 
 class AdSpaceCouponTest extends TestCase
 {
-    public function testGetters()
+    /**
+     * @return void
+     * @dataProvider gettersDataProvider
+     * @throws Exception
+     */
+    public function testGetters(array $coupon)
     {
-        $singleCouponResponseJson = <<<JSON
-{"status":"active","rating":"4.82","campaign":{"id":777,"name":"Name","site_url":"https://www.example.com/"},"description":"","short_name":"Short name","exclusive":false,"date_end":"2024-05-31T23:59:00","date_start":"2024-03-01T00:00:00","id":3,"regions":["RU"],"discount":"500 ₽","types":[{"id":2,"name":"Full name"}],"image":"http://cdn.admitad.com/campaign/images/2021/4/5/tratata.svg","species":"promocode","categories":[{"id":1,"name":"Toys, Kids & Babies"}],"name":"Name","language":"ru","is_unique":false,"is_personal":false,"promocode":"promocode","frameset_link":"https://example.com/coupon/jgmk2fspjv767c32d944748f778371/?erid=adF","goto_link":"https://example.com/g/asd/?i=3&erid=adF"}
-JSON;
+        $dto = new AdSpaceCoupon($coupon);
 
-        $dto = new AdSpaceCoupon(json_decode($singleCouponResponseJson, true));
+        $this->assertSame($coupon['status'], $dto->getStatus());
+        $this->assertSame((float)$coupon['rating'], $dto->getRating());
+        $this->assertSame($coupon['description'], $dto->getDescription());
+        $this->assertSame($coupon['short_name'], $dto->getShortName());
 
-        $this->assertSame('active', $dto->getStatus());
-        $this->assertSame(4.82, $dto->getRating());
-        $this->assertSame('', $dto->getDescription());
-        $this->assertSame('Short name', $dto->getShortName());
-        $this->assertFalse($dto->isExclusive());
-        $this->assertEquals(new \DateTime('2024-05-31T23:59:00'), $dto->getDateEnd());
-        $this->assertEquals(new \DateTime('2024-03-01T00:00:00'), $dto->getDateStart());
-        $this->assertSame(3, $dto->getId());
-        $this->assertEquals(['RU'], $dto->getRegions());
-        $this->assertSame('500 ₽', $dto->getDiscount());
-        $this->assertSame('http://cdn.admitad.com/campaign/images/2021/4/5/tratata.svg', $dto->getImage());
-        $this->assertSame('promocode', $dto->getSpecies());
-        $this->assertSame('Name', $dto->getName());
-        $this->assertSame('ru', $dto->getLanguage());
-        $this->assertFalse($dto->isUnique());
-        $this->assertFalse($dto->isPersonal());
-        $this->assertSame('https://example.com/coupon/jgmk2fspjv767c32d944748f778371/?erid=adF', $dto->getFramesetLink());
-        $this->assertSame('https://example.com/g/asd/?i=3&erid=adF', $dto->getGotoLink());
-        $this->assertSame('promocode', $dto->getPromocode());
+        $this->assertIsBool($dto->isExclusive());
+        $this->assertSame($coupon['exclusive'], $dto->isExclusive());
+
+        if (is_null($coupon['date_end'])) {
+            $this->assertNull($dto->getDateEnd());
+        } else {
+            $this->assertEquals(new \DateTime($coupon['date_end']), $dto->getDateEnd());
+        }
+
+        $this->assertEquals(new \DateTime($coupon['date_start']), $dto->getDateStart());
+        $this->assertSame($coupon['id'], $dto->getId());
+        $this->assertEquals($coupon['regions'], $dto->getRegions());
+        $this->assertSame($coupon['discount'], $dto->getDiscount());
+        $this->assertSame($coupon['image'], $dto->getImage());
+        $this->assertSame($coupon['species'], $dto->getSpecies());
+        $this->assertSame($coupon['name'], $dto->getName());
+        $this->assertSame($coupon['language'], $dto->getLanguage());
+
+        $this->assertIsBool($dto->isUnique());
+        $this->assertSame($coupon['is_unique'], $dto->isUnique());
+
+        $this->assertIsBool($dto->isPersonal());
+        $this->assertSame($coupon['frameset_link'], $dto->getFramesetLink());
+        $this->assertSame($coupon['goto_link'], $dto->getGotoLink());
+        $this->assertSame($coupon['promocode'], $dto->getPromocode());
 
         $this->assertInstanceOf(Campaign::class, $dto->getCampaign());
-        $this->assertSame(777, $dto->getCampaign()->getId());
-        $this->assertSame('Name', $dto->getCampaign()->getName());
-        $this->assertSame('https://www.example.com/', $dto->getCampaign()->getSiteUrl());
+        $this->assertSame($coupon['campaign']['id'], $dto->getCampaign()->getId());
+        $this->assertSame($coupon['campaign']['name'], $dto->getCampaign()->getName());
+        $this->assertSame($coupon['campaign']['site_url'], $dto->getCampaign()->getSiteUrl());
 
         $this->assertIsArray($dto->getTypes());
-        $this->assertCount(1, $dto->getTypes());
-        $this->assertInstanceOf(Type::class, $dto->getTypes()[0]);
-        $this->assertSame(2, $dto->getTypes()[0]->getId());
-        $this->assertSame('Full name', $dto->getTypes()[0]->getName());
+        $this->assertCount(count($coupon['types']), $dto->getTypes());
+        foreach ($dto->getTypes() as $i => $type) {
+            $this->assertInstanceOf(Type::class, $type);
+            $this->assertSame($coupon['types'][$i]['id'], $type->getId());
+            $this->assertSame($coupon['types'][$i]['name'], $type->getName());
+        }
 
         $this->assertIsArray($dto->getCategories());
-        $this->assertCount(1, $dto->getCategories());
-        $this->assertInstanceOf(Category::class, $dto->getCategories()[0]);
-        $this->assertSame(1, $dto->getCategories()[0]->getId());
-        $this->assertSame('Toys, Kids & Babies', $dto->getCategories()[0]->getName());
+        $this->assertCount(count($coupon['categories']), $dto->getCategories());
+        foreach ($dto->getCategories() as $i => $category) {
+            $this->assertInstanceOf(Category::class, $category);
+            $this->assertSame($coupon['categories'][$i]['id'], $category->getId());
+            $this->assertSame($coupon['categories'][$i]['name'], $category->getName());
+        }
+    }
+
+
+    static function gettersDataProvider(): Generator
+    {
+        foreach (json_decode(
+            file_get_contents(__DIR__ . '/fixtures/adspace-coupons.json'),
+            true
+        ) as $coupon) {
+            yield [ $coupon ];
+        }
     }
 }
